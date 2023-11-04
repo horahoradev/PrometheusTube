caaaa791f7c00b7c40877b0b92b679f7cdf3b3c5
OtoMAN <horahora1567@gmail.com>
2023-11-04 12:24:18 -0700

    Humble beginnings

---
+++ frontend/app/root.jsx
@@ -0,0 +1,27 @@
+import {
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+  } from "@remix-run/react";
+  
+  export default function App() {
+    return (
+      <html>
+        <head>
+          <link
+            rel="icon"
+            href="data:image/x-icon;base64,AA"
+          />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <h1>Hello world!</h1>
+          <Outlet />
+  
+          <Scripts />
+        </body>
+      </html>
+    );
+  }
