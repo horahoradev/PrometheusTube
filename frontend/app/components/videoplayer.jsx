9f1e93e72d522cbb38dcf7c5316e581bdf649b25
OtoMAN <horahora1567@gmail.com>
2023-11-06 20:55:18 -0800

    Video metadata progress

--- frontend/app/components/videoplayer.jsx
+++ frontend/app/components/videoplayer.jsx
@@ -4,6 +4,7 @@ import "video.js/dist/video-js.css";
 import Avatar from "@mui/material/Avatar";
 import { HandThumbUpIcon, EyeIcon } from "@heroicons/react/24/outline";
 import { Chip } from "@mui/material";
+import { NavLink } from "@remix-run/react";
 export const VideoPlayer = (props) => {
   const videoRef = React.useRef(null);
   const playerRef = React.useRef(null);
@@ -52,25 +53,31 @@ export const VideoPlayer = (props) => {
         <div ref={videoRef} />
       </Suspense>
       {/* video controls */}
-      <div className="bg-white-300">
-        <span className="text-heading-3">Video Title</span>
+      <div className="bg-white-300 p-2">
+        <span className="text-special-heading-4">Video Title</span>
         <br></br>
         <div className="inline-block mt-3">
           <div className="float-left inline-block">
-            <Avatar sx={{ width: 40, height: 40 }}>N</Avatar>
+            <Avatar sx={{ width: 60, height: 60 }}>N</Avatar>
           </div>
           {/* whatever nerd... (FIXME) */}
-          <div className="inline-block align-middle my-1 ml-1">
-            <div className="text-white-900 font-bold leading-3">Cocanut</div>
+          <div className="inline-block align-middle my-3 ml-1">
+            <div className="text-white-900 font-bold leading-3 text-text-single-400">
+              Cocanut
+            </div>
             {/* <br></br> */}
-            <div className="text-white-800">967k followers</div>
+            <div className="text-white-800 text-text-single-200">
+              967k followers
+            </div>
           </div>
           <span className="ml-3 align-top">
-            <Chip label="Follow" color="secondary" variant="outlined" />
+            <button className="rounded-full p-3 border-cherry-red-200 text-cherry-red-200  border-2	">
+              Follow
+            </button>
           </span>
         </div>
         <br></br>
-        <span className="align-center inline-block mt-3">
+        <span className="align-center inline-block mt-3 text-text-single-300">
           <HandThumbUpIcon className="w-4 inline-block relative align-middle" />
           <span className="align-middle ml-1">0</span>
           <EyeIcon className="ml-4 w-4 inline-block relative align-middle" />
@@ -78,6 +85,17 @@ export const VideoPlayer = (props) => {
           <span className="ml-4">Anime</span>
           <span className="ml-4">Uploaded 05/12/23</span>
         </span>
+        <div className="mt-3">
+          <NavLink to="/?search=Anime" className="text-cherry-red-100">
+            #Anime
+          </NavLink>
+          <NavLink to="/?search=OtoMAD" className=" ml-2 text-cherry-red-100">
+            #OtoMAD
+          </NavLink>
+          <NavLink to="/?search=gaming" className="ml-2 text-cherry-red-100">
+            #gaming
+          </NavLink>
+        </div>
       </div>
     </div>
   );
