ed8b56664009b1af8ae5889ffafa73ae30bdd41b
horahoradev <horahora1567@gmail.com>
2023-11-11 12:44:54 -0800

    add unstaged video components

---
+++ frontend/app/components/videocard.tsx
@@ -0,0 +1,125 @@
+import { NavLink, useSearchParams} from "@remix-run/react";
+import { HandThumbUpIcon, EyeIcon } from "@heroicons/react/24/outline";
+import Avatar from "@mui/material/Avatar";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import { useTheme } from "@mui/material/styles";
+import React, { Suspense } from "react";
+import { useLoaderData } from "@remix-run/react";
+
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useApi } from "~/lib/oapi";
+import { Videos200ResponseVideosInner } from "node_modules/promtube-backend";
+import { json } from "@remix-run/node";
+
+
+
+export default function VideocardList({videos}) {
+  return (
+    <ImageList
+      className="mt-3 overflow-visible index-video-list"
+      // rowHeight={350}
+      // rowWidth={450}
+      gap={6}
+    >
+      {videos === undefined ? <div></div> : videos.map((item) => (
+        <ImageListItem key={item.videoID}>
+          <img
+            className="rounded-lg !w-128 !h-64"
+            srcSet={`${item.thumbnailLoc}`}
+            src={`${item.thumbnailLoc}`}
+            alt={item.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            subtitle={
+              <span>
+                <NavLink to={"/video/" + item.videoID}>
+                  <div className="font-semibold text-white-900 text-text-single-400">
+                    {item.title}
+                  </div>
+                </NavLink>
+                <div className="mt-1 text-white-800 text-text-single-400 font-medium">
+                  <NavLink to={'/profile/' + item.authorID}>{item.authorName}</NavLink>
+                </div>
+                <NavLink to={"/video/" + item.videoID}>
+                  <div className="mt-1 text-white-700 text-text-single-upper-200">
+                    <HandThumbUpIcon className="w-5 inline-block relative align-middle" />
+                    <span className="align-middle ml-1">{item.rating}</span>
+                    <EyeIcon className="ml-2 w-5 inline-block relative align-middle" />
+                    <span className="align-middle ml-1">{item.views}</span>
+                  </div>
+                </NavLink>
+              </span>
+            }
+            position="below"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    author: "bkristastucchio",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+    author: "rollelflex_graphy726",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+    author: "helloimnik",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+    author: "nolanissac",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+    author: "hjrc33",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+    author: "arwinneil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+    author: "tjdragotta",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+    author: "katie_wasserman",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+    author: "silverdalex",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+    author: "shelleypauls",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+    author: "peterlaster",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+    author: "southside_customs",
+  },
+];
---
+++ frontend/app/components/videoplayer.tsx
@@ -0,0 +1,107 @@
+import React, { Suspense } from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+import Avatar from "@mui/material/Avatar";
+import { HandThumbUpIcon, EyeIcon } from "@heroicons/react/24/outline";
+import Comments from "app/components/comments";
+import { NavLink } from "@remix-run/react";
+import { VideoDetail200Response } from "node_modules/promtube-backend";
+
+export const VideoPlayer = (props) => {
+  const videoRef = React.useRef(null);
+  const playerRef = React.useRef(null);
+  let video : VideoDetail200Response = props.video;
+  const { options, onReady } = props;
+
+  React.useEffect(() => {
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
+      const videoElement = document.createElement("video-js");
+
+      // videoElement.classList.add("vjs-big-play-centered");
+      videoElement.classList.add("vjs-16-9");
+      videoRef.current.appendChild(videoElement);
+
+      const player = (playerRef.current = videojs(videoElement, options, () => {
+        videojs.log("player is ready");
+        onReady && onReady(player);
+      }));
+
+      // You could update an existing player in the `else` block here
+      // on prop change, for example:
+    } else {
+      const player = playerRef.current;
+
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
+    }
+  }, [options, videoRef]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  React.useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return (
+    <div data-vjs-player>
+      <Suspense>
+        <div ref={videoRef} />
+      </Suspense>
+      {/* video controls */}
+      <div className="bg-white-300 p-2">
+        <span className="text-special-heading-4">{video.title}</span>
+        <br></br>
+        <div className="inline-block mt-3">
+          <NavLink to={"/profile/" + video.authorID}>
+            <div className="float-left inline-block">
+              <Avatar sx={{ width: 60, height: 60 }}>N</Avatar>
+            </div>
+            {/* whatever nerd... (FIXME) */}
+            <div className="inline-block align-middle my-3 ml-1">
+              <div className="text-white-900 font-bold leading-3 text-text-single-400">
+                {video.username}
+              </div>
+              <div className="text-white-800 text-text-single-200">
+                0 followers
+              </div>
+            </div>
+          </NavLink>
+          <span className="ml-3 align-top">
+            <button className="rounded-full p-3 border-cherry-red-200 text-cherry-red-200 border-2	">
+              Follow
+            </button>
+          </span>
+        </div>
+        <br></br>
+        <span className="align-center inline-block mt-3 text-text-single-300">
+          <HandThumbUpIcon className="w-4 inline-block relative align-middle" />
+          <span className="align-middle ml-1">0</span>
+          <EyeIcon className="ml-4 w-4 inline-block relative align-middle" />
+          <span className="align-middle ml-1">0</span>
+          {/* TODO */}
+          <span className="ml-4">{"OtoMAD"}</span>
+          <span className="ml-4">Uploaded {video.uploadDate}</span>
+        </span>
+        <div className="mt-3">
+        {video.tags?.map((item) => (
+          <NavLink to={"/?search=" + item} className="ml-2 text-cherry-red-100">
+            {'#' + item}
+          </NavLink>
+        ))}
+
+        </div>
+        </div>
+      <Comments></Comments>
+    </div>
+  );
+};
+
+export default VideoPlayer;
