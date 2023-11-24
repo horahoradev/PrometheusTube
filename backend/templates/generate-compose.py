7bcddb09feaf33384686081470785108129a9e68
horahoradev <horahora1567@gmail.com>
2023-11-24 15:55:43 -0800

    wtf

--- backend/templates/generate-compose.py
+++ backend/templates/generate-compose.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
 
     # compose file
     env = Environment(loader=FileSystemLoader("./templates"))
-    template = env.get_template("docker-compose.yaml.j2")
-    f_template = open("docker-compose.yaml", "w")
+    template = env.get_template("docker-compose.yml.j2")
+    f_template = open("docker-compose.yml", "w")
     f_template.write(template.render(build_images=True))
-    print("Wrote docker-compose.yaml")
+    print("Wrote docker-compose.yml")
 
     # nginx config
     template = env.get_template("nginx.conf.j2")
